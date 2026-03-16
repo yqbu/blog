ce74fa7210c77d442b18bb8969cb4547d405d8e4
YQBu <62592769+yqbu@users.noreply.github.com>
2026-03-16 16:01:13 +0800

    Update nav-card.tsx

--- src/components/nav-card.tsx
+++ src/components/nav-card.tsx
@@ -36,25 +36,25 @@ const list = [
 		iconActive: ProjectsFilledSVG,
 		label: '我的项目',
 		href: '/projects'
-	},
-	{
-		icon: AboutOutlineSVG,
-		iconActive: AboutFilledSVG,
-		label: '关于网站',
-		href: '/about'
-	},
-	{
-		icon: ShareOutlineSVG,
-		iconActive: ShareFilledSVG,
-		label: '推荐分享',
-		href: '/share'
-	},
-	{
-		icon: WebsiteOutlineSVG,
-		iconActive: WebsiteFilledSVG,
-		label: '优秀博客',
-		href: '/bloggers'
 	}
+	// ,{
+	// 	icon: AboutOutlineSVG,
+	// 	iconActive: AboutFilledSVG,
+	// 	label: '关于网站',
+	// 	href: '/about'
+	// },
+	// {
+	// 	icon: ShareOutlineSVG,
+	// 	iconActive: ShareFilledSVG,
+	// 	label: '推荐分享',
+	// 	href: '/share'
+	// },
+	// {
+	// 	icon: WebsiteOutlineSVG,
+	// 	iconActive: WebsiteFilledSVG,
+	// 	label: '优秀博客',
+	// 	href: '/bloggers'
+	// }
 ]
 
 const extraSize = 8
